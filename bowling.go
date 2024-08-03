36789285e764cb0fa563dcd4e89f704b50e0721a
Jorge Gueorguiev Garcia <yefo.akira@twoormore.eu>
2024-08-03 13:28:50 +0100

    Completed logic

--- bowling.go
+++ bowling.go
@@ -10,19 +10,50 @@ func Roll(roll int) {
 
 func Score() int {
 	result := 0
+	newFrame := true
+	numberOfFrames := 1
 	for i, v := range scores {
+		if numberOfFrames > 10 {
+			break
+		}
+		if v == -1 {
+			newFrame = !newFrame
+			if newFrame {
+				numberOfFrames++
+			}
+			continue
+		}
+
 		result += v
 		if v == 10 {
-			result += scores[i+1]
-			result += scores[i+2]
+			if scores[i+1] != -1 {
+				result += scores[i+1]
+			}
+			if scores[i+2] != -1 {
+				result += scores[i+2]
+			}
+			newFrame = true
+		} else {
+			if !newFrame {
+				if v+scores[i-1] == 10 {
+					if scores[i+1] != -1 {
+						result += scores[i+1]
+					}
+				}
+			}
+			newFrame = !newFrame
+		}
+		if newFrame {
+			numberOfFrames++
 		}
 	}
+
 	return result
 }
 
 func StartNewGame() {
 	for i := range scores {
-		scores[i] = 0
+		scores[i] = -1
 	}
 	current = 0
 }
--- bowling_test.go
+++ bowling_test.go
@@ -43,7 +43,58 @@ func (suite *BowlingTestSuite) Test_WithStrikeAndWithSingleFollowUpRoll() {
 	assert.Equal(suite.T(), 18, bowling.Score())
 }
 
-func (suite *BowlingTestSuite) Test_WithStrikeAndWithNoFollowUPRoll() {
+func (suite *BowlingTestSuite) Test_WithStrikeAndWithNoFollowUpRoll() {
 	bowling.Roll(10)
 	assert.Equal(suite.T(), 10, bowling.Score())
+}
+
+func (suite *BowlingTestSuite) Test_WithSpareAndFollowUpRoll() {
+	bowling.Roll(4)
+	bowling.Roll(6)
+	bowling.Roll(3)
+	assert.Equal(suite.T(), 16, bowling.Score())
+}
+
+func (suite *BowlingTestSuite) Test_WithSpareNoFollowUpRoll() {
+	bowling.Roll(4)
+	bowling.Roll(6)
+	assert.Equal(suite.T(), 10, bowling.Score())
+}
+
+func (suite *BowlingTestSuite) Test_WithFullGame() {
+	bowling.Roll(10)
+	bowling.Roll(4)
+	bowling.Roll(3)
+	bowling.Roll(8)
+	bowling.Roll(2)
+	bowling.Roll(10)
+	bowling.Roll(8)
+	bowling.Roll(1)
+	bowling.Roll(3)
+	bowling.Roll(3)
+	bowling.Roll(10)
+	bowling.Roll(10)
+	bowling.Roll(10)
+	bowling.Roll(6)
+	bowling.Roll(3)
+	assert.Equal(suite.T(), 162, bowling.Score())
+}
+
+func (suite *BowlingTestSuite) Test_WithAllStrikes() {
+	for i := 0; i < 12; i++ {
+		bowling.Roll(10)
+	}
+
+	assert.Equal(suite.T(), 300, bowling.Score())
+}
+
+func (suite *BowlingTestSuite) Test_StrikeOnFinalFrame() {
+	for i := 0; i < 18; i++ {
+		bowling.Roll(1)
+	}
+	bowling.Roll(10)
+	bowling.Roll(10)
+	bowling.Roll(10)
+
+	assert.Equal(suite.T(), 48, bowling.Score())
 }
