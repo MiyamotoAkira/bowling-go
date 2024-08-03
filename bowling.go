1d0ac6fcd0d2da9472e86b45d40f811741132b4a
Jorge Gueorguiev Garcia <yefo.akira@twoormore.eu>
2024-08-03 13:35:07 +0100

    Adding comments

--- bowling.go
+++ bowling.go
@@ -1,5 +1,6 @@
 package bowling
 
+// 21 represents the maximum amount of possible rolls that could happen
 var scores [21]int
 var current int
 
@@ -13,9 +14,12 @@ func Score() int {
 	newFrame := true
 	numberOfFrames := 1
 	for i, v := range scores {
+		// We have already calculated all possible frames
 		if numberOfFrames > 10 {
 			break
 		}
+
+		// we skip calculation of unused roll/frames
 		if v == -1 {
 			newFrame = !newFrame
 			if newFrame {
@@ -24,8 +28,10 @@ func Score() int {
 			continue
 		}
 
+		// Generally we add the current value
 		result += v
 		if v == 10 {
+			// This branch represents strikes
 			if scores[i+1] != -1 {
 				result += scores[i+1]
 			}
@@ -35,14 +41,21 @@ func Score() int {
 			newFrame = true
 		} else {
 			if !newFrame {
+				// You can only see if is spare on the second roll
+				// of the frame
 				if v+scores[i-1] == 10 {
+					// this branch represents spares
 					if scores[i+1] != -1 {
 						result += scores[i+1]
 					}
 				}
 			}
+
+			// We toggle the beginning of the frame marker
 			newFrame = !newFrame
 		}
+
+		// We increment the current frame number
 		if newFrame {
 			numberOfFrames++
 		}
@@ -53,6 +66,8 @@ func Score() int {
 
 func StartNewGame() {
 	for i := range scores {
+		// -1 represents a sentinel value
+		// No roll has been made yet
 		scores[i] = -1
 	}
 	current = 0
